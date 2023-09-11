e461ec00c34aef27526b3f4df169a8c61af981d3
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-09-11 10:50:42 +0300

    Add logic for changing the state of the Dark Mode

--- simple-portfolio/src/App.js
+++ simple-portfolio/src/App.js
@@ -5,12 +5,22 @@ import Home from './components/Home';
 import Services from './components/Services';
 import WebSites from './components/WebSites';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  function handleClick(){
+    setDarkMode(!darkMode);
+    console.log(darkMode);
+  }
+
   return (
-    <div >
+    <div className={darkMode ? "dark" : ""}>
 
       <div className="min-h-screen transition-all duration-500  dark:bg-sky-900">
-            <Navigation />
+            <Navigation darkModeHandling = {handleClick} />
             <Home />
             <Services />
             <WebSites />
--- simple-portfolio/src/components/Navigation.js
+++ simple-portfolio/src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Navigation() {
+function Navigation({darkModeHandling}) {
     return (
         
         <div className="flex p-10 justify-between items-center transition-all duration-500 dark:bg-sky-900">
             <p className="font-bold tracking-widest dark:text-white">NordDevelopers</p>
 
             <div className="flex justify-center gap-8 items-center mr-20">
-                <i className="fa-solid fa-moon text-3xl dark:text-white cursor-pointer" onClick={() => setDarkMode(!darkMode)}></i>
+                <i className="fa-solid fa-moon text-3xl dark:text-white cursor-pointer" onClick={darkModeHandling}></i>
                 <button className="bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-1 rounded-md text-white">Resume</button>
             </div>
         </div>
