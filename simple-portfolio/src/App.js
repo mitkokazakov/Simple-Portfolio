3b3ff5e3eac4086800f282b6fb401d35f0f10d8d
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-09-11 09:09:37 +0300

    Add WebSites component

--- simple-portfolio/src/App.js
+++ simple-portfolio/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Services from './components/Services';
+import WebSites from './components/WebSites';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Navigation />
             <Home />
             <Services />
+            <WebSites />
       </div>
       
     </div>
---
+++ simple-portfolio/src/components/WebSites.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+import web1 from '../images/web1.png'
+import web2 from '../images/web2.png'
+import web3 from '../images/web3.png'
+import web4 from '../images/web4.png'
+import web5 from '../images/web5.png'
+import web6 from '../images/web6.png'
+
+function WebSites() {
+    return (
+
+        <div className='px-16 pt-10'>
+
+            <h3 className='text-3xl text-left dark:text-white'>Portfolio</h3>
+
+            <p className='text-left pt-5 dark:text-white'>
+                Since the beginning of my journey as a freelance designer and
+                developer, I've done remote work for
+                <span className="text-teal-500"> agencies </span>
+                consulted for <span className="text-teal-500">startups </span>
+                and collaborated with talanted people to create digital products
+                for both business and consumer use.
+            </p>
+
+            <p className='text-left py-5 dark:text-white'>I offer from a wide range of services, including brand design,
+                programming and teaching.</p>
+
+            <div className='flex gap-10 pb-10 flex-wrap'>
+
+                <div className='basis-1/3 flex-1'>
+                    <img className='w-full h-full rounded-lg' src={web1}></img>
+                </div>
+
+                <div className='basis-1/3 flex-1'>
+                    <img className='w-full h-full rounded-lg' src={web2}></img>
+                </div>
+
+                <div className='basis-1/3 flex-1'>
+                    <img className='w-full h-full rounded-lg' src={web3}></img>
+                </div>
+
+                <div className='basis-1/3 flex-1'>
+                    <img className='w-full h-full rounded-lg' src={web4}></img>
+                </div>
+
+                <div className='basis-1/3 flex-1'>
+                    <img className='w-full h-full rounded-lg' src={web5}></img>
+                </div>
+
+                <div className='basis-1/3 flex-1'>
+                    <img className='w-full h-full rounded-lg' src={web6}></img>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default WebSites
